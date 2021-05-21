c0d29707d224ae34a13e4b3656c99fffc2e4dbef
Matthew Brown <mbrown@geomarvel.com>
2021-05-20 20:58:58 -0400

    safe area and status bar fix

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar as ExpoSB } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Our react native canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoSB style="auto" />
+    </>
   );
 }
 
