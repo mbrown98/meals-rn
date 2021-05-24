491b7b3469805799070efb49c6ded5a88522d3c8
Matthew Brown <mbrown@geomarvel.com>
2021-05-23 22:20:55 -0400

    fully styled card

--- src/features/restaurants/components/RestaurantInfoCardComponent.js
+++ src/features/restaurants/components/RestaurantInfoCardComponent.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
 import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
-import star from "../../../../assets/star.js";
-import open from "../../../../assets/open.js";
 
-const RestaurantCard = styled(Card)``;
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
 
 const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
-  background-color: white;
-`;
-
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-
-  color: ${(props) => props.theme.colors.ui.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Address = styled(Text)`
@@ -25,61 +21,73 @@ const Address = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
-const Info = styled(View)`
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const Rating = styled(View)`
+const Rating = styled.View`
   flex-direction: row;
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
-
-const Section = styled(View)`
+const Section = styled.View`
   flex-direction: row;
   align-items: center;
 `;
-
-const SectionEnd = styled(View)`
+const SectionEnd = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: flex-end;
 `;
 
-const RestaurantInfoCardComponent = ({ restaurant = {} }) => {
+const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
-      "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
-    address = "100 Random St.",
+    address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <RestaurantCard>
+    <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
         <Section>
           <Rating>
-            {ratingArray.map((rating) => (
-              <SvgXml xml={star} width={20} height={20} key={rating} />
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
             ))}
           </Rating>
           <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <View style={{ paddingLeft: 16 }} />
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View style={{ paddingLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
-
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   );
 };
 
-export default RestaurantInfoCardComponent;
+export default RestaurantInfoCard;
